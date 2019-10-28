55a0e9874805a682172e68abb7099a5d55fc8bff
Jason Cortella <jason.r.cortella@gmail.com>
2019-10-28 11:59:53 -0700

    Refactor

--- SFconcerts.py
+++ SFconcerts.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 #SETTINGS ------------------------------------------------------------------------------------------------------------------------
 
-daysToDisplay = 2  # Select the number of days of concerts to display.
+daysToDisplay = 10  # Select the number of days of concerts to display.
                    # Currently, can only display through next Sunday, such that the max is 8-14 depending on day of the week
 
 cities = []        # Select cities to display. Comment out undesired cities
@@ -21,27 +21,27 @@ cities.append('S.F.')
 
 day = datetime.now().weekday()
 start = 0 if day != 0 else 1 # If the script is ran on a monday, the second page must be used due to the site's update frequency
-url1 = "http://www.foopee.com/punk/the-list/by-date.{}.html".format(start) # current week
-url2 = "http://www.foopee.com/punk/the-list/by-date.{}.html".format(start + 1) # next week
 
+url1 = "http://www.foopee.com/punk/the-list/by-date.{}.html".format(start) # current week
 page1 = requests.get(url1)
 soup1 = BeautifulSoup(page1.text, 'html.parser')
 indexList1 = []
-x1 = soup1.find_all('a')
-for i, item in enumerate(x1): # Find date separators
+response1 = soup1.find_all('a')
+for i, item in enumerate(response1): # Find date separators
     if 'name="' in str(item):
         indexList1.append(i)
-indexList1.append(len(x1)) # Set endpoint
+indexList1.append(len(response1)) # Set endpoint
 
 if (daysToDisplay > (7 - day)): # Skip second GET request if not required
+    url2 = "http://www.foopee.com/punk/the-list/by-date.{}.html".format(start + 1) # next week
     page2 = requests.get(url2)
     soup2 = BeautifulSoup(page2.text, 'html.parser')
-    x2 = soup2.find_all('a')
+    response2 = soup2.find_all('a')
     indexList2 = []
-    for i, item in enumerate(x2):
+    for i, item in enumerate(response2):
         if 'name="' in str(item):
             indexList2.append(i)
-    indexList2.append(len(x2))
+    indexList2.append(len(response2))
 
 printed = 0
 Flag = False
@@ -49,32 +49,32 @@ for j in range(7 - day): # Displays remaining concerts for the week
     if printed >= daysToDisplay:
         break
     for i in range (indexList1[j], indexList1[j+1]):
-        a = (2 if i == indexList1[j] else 1) # if on a date field, selects date, otherwise selects venue / artist
-        z = str(x1[i]).split(">")[a].split("<")[0]
-        if '<a name="' in str(x1[i]):
-            print(z)
-        if '<a href="by-club' in str(x1[i]):
-            Flag = True if any([city in z for city in cities]) else False # only display the concerts in selected cities
+        field = (2 if i == indexList1[j] else 1) # if on field date field, selects date, otherwise selects venue / artist
+        text = str(response1[i]).split(">")[field].split("<")[0]
+        if '<field name="' in str(response1[i]):
+            print(text)
+        if '<a href="by-club' in str(response1[i]):
+            Flag = True if any([city in text for city in cities]) else False # only display the concerts in selected cities
             if Flag:
-                print("\t" + z)
-        if '<a href="by-band' in str(x1[i]):
+                print("\t" + text)
+        if '<a href="by-band' in str(response1[i]):
             if Flag:
-                print("\t\t" + z)
+                print("\t\t" + text)
     printed += 1
 Flag = False
 for j in range(7):
     if printed >= daysToDisplay:
         break
     for i in range (indexList2[j], indexList2[j+1]):
-        a = (2 if i == indexList2[j] else 1) # if on a date field, selects date, otherwise selects venue / artist
-        z = str(x2[i]).split(">")[a].split("<")[0]
-        if '<a name="' in str(x2[i]):
-            print(z)
-        if '<a href="by-club' in str(x2[i]):
-            Flag = True if any([city in z for city in cities]) else False # only display the concerts in selected cities
+        field = (2 if i == indexList2[j] else 1) # if on field date field, selects date, otherwise selects venue / artist
+        text = str(response2[i]).split(">")[field].split("<")[0]
+        if '<a name="' in str(response2[i]):
+            print(text)
+        if '<a href="by-club' in str(response2[i]):
+            Flag = True if any([city in text for city in cities]) else False # only display the concerts in selected cities
             if Flag:
-                print("\t" + z)
-        if '<a href="by-band' in str(x1[i]):
+                print("\t" + text)
+        if '<a href="by-band' in str(response2[i]):
             if Flag:
-                print("\t\t" + z)
+                print("\t\t" + text)
     printed += 1
